ca7d1d188e34f58d578566fdf36b05bc6632bbca
root <rajub@alphabittech.com>
2018-08-15 22:54:13 +0530

    fitcommit

---
+++ src/main/java/amit[C[C[C[C[C[C[C[C[Ch[C[C[C[C[C[C/App.java
@@ -0,0 +1,13 @@
+package amit[C[C[C[C[C[C[C[C[Ch[C[C[C[C[C[C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
